be77e76650dd09e3002ccbb6e97d62d08fde0a30
Milli <deme23ke@student.ju.se>
2026-03-06 11:44:17 +0100

    Finally fixed store filtering and connected it to server.js
    Had to rearrange some of the files but they are still your files 🐖

--- public/start.js
+++ public/start.js
@@ -1,27 +1,54 @@
-// 1. Fetch the data
+//Getting the stores from the server
 fetch("/api/venues")
-  .then((response) => response.json())
+  .then((res) => res.json())
   .then((data) => {
+    // Sorting stores alphabetically
     const sortedData = data.sort((a, b) => a.name.localeCompare(b.name, "sv"));
-    renderSimpleList(sortedData);
+
+    sortedData.forEach((store) => {
+      const firstLetter = store.name.charAt(0).toUpperCase();
+      const container = document.querySelector(
+        `#section${firstLetter} + .stores`,
+      );
+
+      if (container) {
+        const p = document.createElement("p");
+        p.style.padding = "5px 20px";
+
+        // Creating the link for the stores
+        if (store.url) {
+          const link = document.createElement("a");
+          link.href = store.url.startsWith("http")
+            ? store.url
+            : "https://" + store.url;
+          link.target = "_blank";
+          link.innerText = store.name;
+          p.appendChild(link);
+        } else {
+          p.innerText = store.name;
+        }
+
+        container.appendChild(p);
+      }
+    });
+
+    // Creating the click funtions after finding data
+    setupCollapsibles();
   })
-  .catch((err) => console.error("Connection error:", err));
+  .catch((err) => console.error("Error loading venues:", err));
 
-//2. Render it
-function renderSimpleList(venues) {
-  const container = document.getElementById("venue-container");
-  container.innerHTML = "";
-
-  const ul = document.createElement("ul");
-
-  venues.forEach((venue) => {
-    const li = document.createElement("li");
-
-    li.innerText = venue.name;
-
-    ul.appendChild(li);
-  });
-
-  container.appendChild(ul);
+// Click function to open and close
+function setupCollapsibles() {
+  let coll = document.getElementsByClassName("collapsible");
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      let stores = this.nextElementSibling;
+      // Vi kollar display. Om den är "block" döljer vi, annars visar vi
+      if (stores.style.display === "block") {
+        stores.style.display = "none";
+      } else {
+        stores.style.display = "block";
+      }
+    });
+  }
 }
-//CLICK HERE -> http://localhost:3000/ to find the stores page yall
